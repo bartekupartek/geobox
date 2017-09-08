d334b2909713b9b84d8e83ab6e58204e8bd79728
Bartlomiej Rozanski <ebartlomiej@gmail.com>
2017-09-08 20:03:45 +0200

    first commit

---
+++ lib/geobox.ex
@@ -0,0 +1,18 @@
+defmodule Geobox do
+  @moduledoc """
+  Documentation for Geobox.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Geobox.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/geobox_test.exs
@@ -0,0 +1,8 @@
+defmodule GeoboxTest do
+  use ExUnit.Case
+  doctest Geobox
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
